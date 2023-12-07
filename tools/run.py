413380f678219b6166ca2be51bd2d19f7a535915
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-12-07 09:52:53 -0800

    add resnet50

--- tools/run.py
+++ tools/run.py
@@ -1,6 +1,7 @@
 from ai.dive.saver import Saver
 from ai.dive.diver import Diver
 from ai.dive.models.vit import ViT
+from ai.dive.models.resnet50 import ResNet50
 from ai.dive.data.file_classification import FileClassification
 from ai.dive.data.directory_classification import DirectoryClassification
 import argparse
@@ -13,15 +14,15 @@ def main():
     parser.add_argument('-n', '--num-samples', default=-1, type=int, help='Number of samples to run model on')
     args = parser.parse_args()
 
-    model = ViT()
-    # dataset = FileClassification(
-    #     data_dir=args.dataset,
-    #     file="test.csv",
-    #     path_key="filename",
-    #     label_key="class_name",
-    # )
+    model = ResNet50()
+    dataset = FileClassification(
+        data_dir=args.dataset,
+        file="images.csv",
+        path_key="filename",
+        label_key="class_name",
+    )
 
-    dataset = DirectoryClassification(data_dir=args.dataset)
+    # dataset = DirectoryClassification(data_dir=args.dataset)
 
     output_keys = ['filename', 'class_name', 'prediction', 'probability', 'is_correct']
     saver = Saver(args.output, output_keys=output_keys, format="csv", save_every=10)
