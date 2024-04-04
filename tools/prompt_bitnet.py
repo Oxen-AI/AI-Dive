c9c9ff194323440c45fbed45a43a3a9883ba67c6
Greg Schoeninger <greg.schoeninger@gmail.com>
2024-04-04 18:22:59 +0000

    add prompt bitnet

---
+++ tools/prompt_bitnet.py
@@ -0,0 +1,26 @@
+
+import argparse
+from ai.dive.models.bitnet_llm import BitNetLLM
+from ai.dive.prompts.assistant_prompt import AssistantPrompt
+
+def main():
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description='Prompt our BitNet model in a loop.')
+    parser.add_argument('-m', '--model', required=True, type=str, help='dataset to run model on')
+    args = parser.parse_args()
+
+    # create the model
+    model = BitNetLLM(args.model)
+    
+    # prompt the model in a loop
+    while True:
+        prompt = input("🐂 > ")
+        prompt = AssistantPrompt({"prompt": prompt}).render()
+        data = {"prompt": prompt, "answers": []}
+        output = model.predict(data)
+        print(f"Time: {output['time']}s")
+        print()
+    
+
+if __name__ == '__main__':
+    main()
